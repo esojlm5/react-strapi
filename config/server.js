74cd4d47435ef4add1adc485b6943a62f2cd2007
esojlm5 <ne.ver1@outlook.com>
2021-08-12 20:22:46 -0500

    start project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4e429d068915b14ae793a9871e404c7b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5552a1a2-4dce-4969-b3a3-ce09f4cb6cd1'
+};
